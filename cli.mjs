4a8d4d38e2d75fadb49f524614cde75264f3bdc3
Avaray <deffqqq@gmail.com>
2023-11-15 11:41:34 +0100

    from 4 spaces to 2

--- cli.mjs
+++ cli.mjs
@@ -5,59 +5,59 @@ import vernode from './module.mjs';
 const args = process.argv.slice(2)
 
 function Format(data) {
-    if (args.find(arg => /^js/i.test(arg))) {
-        console.log(data)
-    } else {
-        data.lts && console.log(`LTS: ${data.lts}`);
-        data.current && console.log(`Current: ${data.current}`);
-        data.nightly && console.log(`Nightly: ${data.nightly}`);
-    }
+  if (args.find(arg => /^js/i.test(arg))) {
+    console.log(data)
+  } else {
+    data.lts && console.log(`LTS: ${data.lts}`);
+    data.current && console.log(`Current: ${data.current}`);
+    data.nightly && console.log(`Nightly: ${data.nightly}`);
+  }
 }
 
 if (args.length === 0 || args.find(arg => /^js/i.test(arg))) {
-    vernode()
-        .then((x) => Format(x))
-        .catch((err) => console.error(err));
+  vernode()
+    .then((x) => Format(x))
+    .catch((err) => console.error(err));
 } else {
-    switch (args[0].toLowerCase()) {
-        case 'h':
-        case '-h':
-        case 'help':
-        case '-help':
-        case '--help':
-        case 'usage':
-        case '?':
-            HELP();
-            break;
-        case 'l':
-        case 'lts':
-            vernode('lts').then((x) => console.log(x.lts)).catch((err) => console.error(err));
-            break;
-        case 'c':
-        case 'current':
-            vernode('current').then((x) => console.log(x.current)).catch((err) => console.error(err));
-            break;
-        case 'n':
-        case 'nightly':
-            vernode('nightly').then((x) => console.log(x.nightly)).catch((err) => console.error(err));
-            break;
-        default:
-            console.log('Type "vernode help" to see available commands');
-    }
+  switch (args[0].toLowerCase()) {
+    case 'h':
+    case '-h':
+    case 'help':
+    case '-help':
+    case '--help':
+    case 'usage':
+    case '?':
+      HELP();
+      break;
+    case 'l':
+    case 'lts':
+      vernode('lts').then((x) => console.log(x.lts)).catch((err) => console.error(err));
+      break;
+    case 'c':
+    case 'current':
+      vernode('current').then((x) => console.log(x.current)).catch((err) => console.error(err));
+      break;
+    case 'n':
+    case 'nightly':
+      vernode('nightly').then((x) => console.log(x.nightly)).catch((err) => console.error(err));
+      break;
+    default:
+      console.log('Type "vernode help" to see available commands');
+  }
 }
 
 function HELP() {
-    
-    console.log('\nShow all versions (LTS, Current and Nightly)');
-    console.log('Just run this app without arguments\n');
 
-    console.log('Show LTS version (l, lts)');
-    console.log('Example: vernode lts\n');
+  console.log('\nShow all versions (LTS, Current and Nightly)');
+  console.log('Just run this app without arguments\n');
 
-    console.log('Show Current version (c, current)');
-    console.log('Example: vernode current\n');
+  console.log('Show LTS version (l, lts)');
+  console.log('Example: vernode lts\n');
 
-    console.log('Show Nightly version (n, nightly)');
-    console.log('Example: vernode nightly\n');
+  console.log('Show Current version (c, current)');
+  console.log('Example: vernode current\n');
+
+  console.log('Show Nightly version (n, nightly)');
+  console.log('Example: vernode nightly\n');
 
 }
--- module.mjs
+++ module.mjs
@@ -8,42 +8,42 @@ const URL_nightly = 'https://nodejs.org/download/nightly/index.json'
 const versions = {}
 
 const fetch_STABLES = async () => {
-    try {
-        const response = await fetch(URL_stables);
-        const data = await response.json()
-        versions.lts = data.find(e => e.lts != false).version.replace('v','')
-        versions.current = data.find(e => e.lts === false).version.replace('v','')  
-    } catch (error) {
-         /* empty */ 
-    }
+  try {
+    const response = await fetch(URL_stables);
+    const data = await response.json()
+    versions.lts = data.find(e => e.lts != false).version.replace('v', '')
+    versions.current = data.find(e => e.lts === false).version.replace('v', '')
+  } catch (error) {
+    /* empty */
+  }
 }
 
 const fetch_NIGHTLY = async () => {
-    try {
-        const response = await fetch(URL_nightly);
-        const data = await response.json()
-        versions.nightly = data[0].version.replace('v','').split('-')[0]
-    } catch (error) {
-         /* empty */ 
-    }
+  try {
+    const response = await fetch(URL_nightly);
+    const data = await response.json()
+    versions.nightly = data[0].version.replace('v', '').split('-')[0]
+  } catch (error) {
+    /* empty */
+  }
 }
 
-export default async function(x) {
+export default async function (x) {
 
-    switch (x) {
-        case 'lts':
-            await fetch_STABLES()
-            return versions
-        case 'current':
-            await fetch_STABLES()
-            return versions
-        case 'nightly':
-            await fetch_NIGHTLY()
-            return versions
-        default:
-            await fetch_STABLES()
-            await fetch_NIGHTLY()
-            return versions
-    }
+  switch (x) {
+    case 'lts':
+      await fetch_STABLES()
+      return versions
+    case 'current':
+      await fetch_STABLES()
+      return versions
+    case 'nightly':
+      await fetch_NIGHTLY()
+      return versions
+    default:
+      await fetch_STABLES()
+      await fetch_NIGHTLY()
+      return versions
+  }
 
 }
